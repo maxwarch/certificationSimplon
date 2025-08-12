8b0f7d765899b756e10a5cbd1b534f9bec57365f
Maxime KUIL <maximekuil@gmail.com>
2025-08-12 15:42:46 +0200

    optimize entrypoint cron

--- E1/docker/cron-entrypoint.sh
+++ E1/docker/cron-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod u=r,go=r -R /app/refresh_cron.py
-chmod u=r,go=r,a+x -R /app/refresh_cron.py
+chmod u=r,go=r /app/refresh_cron.py
+chmod u=r,go=r,a+x /app/refresh_cron.py
 
 printenv > /etc/environment
 cron -f -l 2 -L /dev/stdout
